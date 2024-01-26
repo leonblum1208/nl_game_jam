7d03d44765fd6872c4d737424f348d2d5ece66a0
Laura <laura.kuper@rwth-aachen.de>
2024-01-26 22:31:03 +0100

    Implement discrete player movement

---
+++ src/game.py
@@ -0,0 +1,22 @@
+import pygame
+import sys
+from pydantic import BaseModel
+from src.player.player import Player
+
+class Game(BaseModel):
+    player: Player
+
+    def handle_event(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.player.move(-1, 0)
+                elif event.key == pygame.K_RIGHT:
+                    self.player.move(1, 0)
+                elif event.key == pygame.K_UP:
+                    self.player.move(0, -1)
+                elif event.key == pygame.K_DOWN:
+                    self.player.move(0, 1)
